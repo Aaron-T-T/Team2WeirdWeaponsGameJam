13502efad028fbe73a2500a45861af091475a206
Gabriel Flores <gravy239@gmail.com>
2024-01-24 15:51:46 -0600

    Wolf attacks
    Finished the wolf enemy behavior script to include its attacks. Also created a folder for animations and added the racoon enemy animation to play on any state.

--- WeirdWeaponsGameJam/Assets/Scripts/Wolf.cs
+++ WeirdWeaponsGameJam/Assets/Scripts/Wolf.cs
@@ -9,17 +9,22 @@ using UnityEngine;
 
 public class Wolf : MonoBehaviour
 {
-    public GameObject start;
-    public GameObject end;
+    public GameObject player;
+    public ParticleSystem wolfBullet;
+
     public float patrolSpeed;
+    public float chaseSpeed;
     public float patrolDistance;
 
-    private Vector3 startingPosition;
     private bool attackMode = false;
     private bool takingAction = false;
+    private bool takingShot = false;
+    private bool takingMelee = false;
+    private int rand;
 
-    private CapsuleCollider detectionSpace;
+    private Vector3 startingPosition;
     private Vector3 patrolDirection = Vector3.forward;
+    private ParticleSystem clonedBullet;
     
     // Start is called before the first frame update
     void Start()
@@ -34,14 +39,24 @@ public class Wolf : MonoBehaviour
         {
             Patrol();
         }
+        else if (attackMode)
+        {
+            Attack();
+        }
+        Debug.Log(rand);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            attackMode = true;
+        }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if(other.tag == "Player")
-        {
-            attackMode = true;
-        }
+        
     }
 
     private void OnTriggerExit(Collider other)
@@ -59,13 +74,69 @@ public class Wolf : MonoBehaviour
         if(transform.position.x > startingPosition.x + patrolDistance)
 
         {
-            Debug.Log("Moving left");
-            patrolDirection = Vector3.back;
+            transform.Rotate(0f, -90f, 0f);
+            patrolDirection = Vector3.forward;
         }
         else if (transform.position.x < startingPosition.x -patrolDistance)
         {
-            Debug.Log("Moving right");
+            transform.Rotate(0f, 90f, 0f);
             patrolDirection = Vector3.forward;
         }
+    }
+
+    private void Attack()
+    {
+        if (!takingAction)
+        {
+            rand = Random.Range(1, 3);
+            takingAction = true;
+        }
+
+        if (rand == 1 && Vector3.Distance(transform.position, player.transform.position) > 3f)
+        {
+            gameObject.transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime * chaseSpeed);
+        }
+        else if (rand == 1 && !takingMelee)
+        {
+            MeleeAttack();
+        }
+        
+        if (rand == 2 && !takingShot)
+        {
+            Shoot();
+        }
+
+    }
+
+    private void MeleeAttack()
+    {
+        takingMelee = true;
+        StartCoroutine(postAttackDelay());    
+    }
+    
+    private void Shoot()
+    {
+        takingShot = true;
+        StartCoroutine(postShotDelay());
+    }
+
+   IEnumerator postAttackDelay()
+    {
+        //Would play attack animeation
+
+        yield return new WaitForSeconds(2f);
+        takingAction = false;
+        takingMelee = false;
+        Debug.Log("ActionChange");
+    }
+
+    IEnumerator postShotDelay()
+    {
+        clonedBullet = Instantiate(wolfBullet, wolfBullet.transform.position, wolfBullet.transform.rotation);
+        clonedBullet.Play();
+        yield return new WaitForSeconds(2f);
+        takingShot = false;
+        takingAction = false;
+        Debug.Log("ActionChange");
     }
 }
